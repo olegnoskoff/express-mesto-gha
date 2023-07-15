b4af58ba91cd49b35d81f09c5efbdca79435fa55
Oleg <olegnoskoff@yandex.ru>
2023-07-15 14:14:29 +0300

    fix bag

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,21 +41,27 @@ async function createUser(req, res) {
   try {
     const { name, about, avatar } = req.body;
 
-    if (name.length < 2) {
+    // Проверка наличия данных в запросе
+    if (!name || !about || !avatar) {
       res.status(STATUS_BAD_REQUEST).send({
-        message: 'Некорректное имя пользователя',
+        message: 'Отсутствуют необходимые данные для регистрации пользователя.',
       });
       return;
     }
 
+    // Создание нового пользователя
     const user = await User.create({ name, about, avatar });
-    res.status(STATUS_CREATED).send(user);
+
+    // Возвращаем созданного пользователя в ответе
+    res.status(STATUS_OK).send(user);
   } catch (err) {
+    // Проверка и обработка ошибок валидации
     if (err.name === 'ValidationError') {
       res.status(STATUS_BAD_REQUEST).send({
         message: err.message,
       });
     } else {
+      // Обработка других ошибок
       res.status(STATUS_INTERNAL_SERVER_ERROR).send({
         message: STATUS_INTERNAL_SERVER_ERROR,
       });
